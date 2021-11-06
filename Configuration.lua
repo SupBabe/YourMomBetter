172345474b89c885d23458137b91ef46cfeb581b
SupBabe <78192622+SupBabe@users.noreply.github.com>
2021-11-06 10:31:23 +0300

    Create Configuration.lua

---
+++ Configuration.lua
@@ -0,0 +1,75 @@
+repeat wait() until game:IsLoaded()
+
+getgenv().Min = 8 -- min players
+getgenv().Max = 12 -- max players
+getgenv().MaxRokas = 25 -- Max Roka
+getgenv().MaxArrows = 25 -- Max Arrows
+getgenv().webhook = "https://discord.com/api/webhooks/901801086711660544/Gi0Uq9pjV_rVBHgwCcHuJdr5h6EmkIEac_dq_g0EGWpY9vz5lOS4prh5EjPeU3mtvh9S" -- your webhook 
+getgenv().DiscordID = "@everyone" -- your discord ID
+getgenv().FastMode = true; -- fast mode if you set that to false you can set delay to hopper Teleporting In Case You Have Problems with client behavior
+getgenv().HopperDelay = 0.1; -- delay hopper the more the slow search
+getgenv().SkipAssets = true; -- make you able to skip assets 
+getgenv().ResetAfter = 10 -- 10 minutes
+getgenv().Farming = {
+    ["Rokakaka"] = true,
+    ["Mysterious Arrow"] = true,
+    ["Lucky Arrow"] = true
+}
+
+getgenv().Shinies = {
+["Ender Crimson"] = true; 
+["Manga Crimson"] = true; 
+["Female The Universe"] = true;
+["Glock-18 | Fade"] = false; 
+["Scarlet Queen"] = true;
+["Shadow The Universe"] = true;
+["Pink Fingers"] = false;
+["The Other Hand"] = true;
+["OVA Grey Rapier"] = true;
+["Magellan"] = true;
+["Heaven's Ember"] = true;
+["Vexus Crimson"] = true;
+["Misaka Mikoto"] = true;
+["Airsmith Over Heaven"] = true;
+["The World 2"] = true;
+["Sasageyo"] = true;
+["Platinum Fingers"] = true;
+["Female Hando"] = true;
+["Kanshou & Bakuya"] = true;
+["Shining Emerald"] = true;
+["Baseball Platinum"] = true; 
+["Shining Ruby"] = true;
+["Hierophant Black"] = true;
+["Silver Experience"] = true;
+["Killer Reveal"] = true;
+["Hierophant Yellow"] = true;
+["Turquoise Platinum"] = true;
+["Kikoku"] = true;
+["Mr. Black&White"] = true;
+["Verdant Vine"] = true;
+["Female D4C"] = true;
+["Chromo Hando"] = true;
+["Lavendersnake"] = true;
+["Verdant Platinum"] = true;
+["Vinegar Crimson"] = true;
+["OVA Platinum"] = true;
+["Mintsnake"] = true; 
+["Uber Spy"] = true;
+["Hierophant Purple"] = true;
+["Elucidator & Dark Repulser"] = true;
+["Glock-18"] = false;
+["Golden Platinum"] = true;
+["Ghost World"] = true;
+["Deimos Queen"] = true;
+["OVA The Universe"] = true;
+["Tentacle Green Alternate Universe"] = true;
+["Jack-O-Platinum"] = true;
+["Cornsnake"] = true;
+["Nerf Jolt"] = false;
+["Creeper Queen"] = false;
+["Megumin"] = false;
+}
+
+-------------
+script here
+-------------
