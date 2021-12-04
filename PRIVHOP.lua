c41041ca3496fb3007a0ea3060a72eba77a616a1
SupBabe <78192622+SupBabe@users.noreply.github.com>
2021-12-04 12:48:08 +0300

    Update PRIVHOP.lua

--- PRIVHOP.lua
+++ PRIVHOP.lua
@@ -5,4 +5,5 @@ _G.ItemFarm = "Lucky Arrow" -- Notify Which Item for webhook
 getgenv().webhook = "" -- leave both empty if you don't want webhook ( otherwise it's required for both if you want webhook )
 getgenv().DiscordID = ""
 getgenv().ItemDelay = 3 -- this is when normally item picked up due to yba lag at least for me just adjust it whatever you want
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SupBabe/YourMomBetter/main/FastHook.lua"))()
 ----
